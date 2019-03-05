c8f1abdbb6e819c8090fa099c52d4532169b32f8
Yveh <1053697976@qq.com>
2019-03-05 14:59:42 +0800

    Update vector.hpp

--- statement/vector/vector.hpp
+++ statement/vector/vector.hpp
@@ -22,7 +22,7 @@ private:
 	void expand()
 	{
 		arrSize *= 2;
-		int *ex_arr = new T[arrSize];
+		T *ex_arr = new T[arrSize];
 		for (int i = 0; i < dataSize; ++i)
 			ex_arr[i] = arr[i];
 		delete []arr;
@@ -40,13 +40,21 @@ public:
 	class const_iterator;
 	class iterator
 	{
+		friend vector;
+	
 	private:
 		/**
 		 * TODO add data members
 		 *   just add whatever you want.
 		 */
 		int index;
+		T *arr;
 	public:
+		iterator(T *_arr = nullptr, int _index = 0)
+		{
+			arr = _arr;
+			index = _index;
+		}
 		/**
 		 * return a new iterator which pointer n-next elements
 		 *   even if there are not enough elements, just return the answer.
@@ -55,12 +63,12 @@ public:
 		iterator operator+(const int &n) const
 		{
 			//TODO
-			return iterator(index + n);
+			return iterator(arr, index + n);
 		}
 		iterator operator-(const int &n) const
 		{
 			//TODO
-			return iterator(index - n);
+			return iterator(arr, index - n);
 		}
 		// return th distance between two iterator,
 		// if these two iterators points to different vectors, throw invaild_iterator.
@@ -73,16 +81,12 @@ public:
 		{
 			//TODO
 			index += n;
-			if (index < 0 || index >= dataSize)
-				throw index_out_of_bound;
 			return *this;
 		}
 		iterator operator-=(const int &n)
 		{
 			//TODO
 			index -= n;
-			if (index < 0 || index >= dataSize)
-				throw index_out_of_bound;
 			return *this;
 		}
 		/**
@@ -91,7 +95,7 @@ public:
 		iterator operator++(int)
 		{
 			++index;
-			return iterator(index);
+			return iterator(arr, index);
 		}
 		/**
 		 * TODO ++iter
@@ -107,7 +111,7 @@ public:
 		iterator operator--(int)
 		{
 			--index;
-			return iterator(index);
+			return iterator(arr, index);
 		}
 		/**
 		 * TODO --iter
@@ -153,19 +157,32 @@ public:
 	 */
 	class const_iterator
 	{
+		friend vector;
+
 	private:
 		int index;
+		T *arr;
 
 	public:
+		const_iterator(T *_arr = nullptr, int _index = 0)
+		{
+			arr = _arr;
+			index = _index;
+		}
+		/**
+		 * return a new iterator which pointer n-next elements
+		 *   even if there are not enough elements, just return the answer.
+		 * as well as operator-
+		 */
 		const_iterator operator+(const int &n) const
 		{
 			//TODO
-			return const_iterator(index + n);
+			return const_iterator(arr, index + n);
 		}
 		const_iterator operator-(const int &n) const
 		{
 			//TODO
-			return const_iterator(index - n);
+			return const_iterator(arr, index - n);
 		}
 		// return th distance between two iterator,
 		// if these two iterators points to different vectors, throw invaild_iterator.
@@ -178,16 +195,12 @@ public:
 		{
 			//TODO
 			index += n;
-			if (index < 0 || index >= dataSize)
-				throw index_out_of_bound;
 			return *this;
 		}
 		const_iterator operator-=(const int &n)
 		{
 			//TODO
 			index -= n;
-			if (index < 0 || index >= dataSize)
-				throw index_out_of_bound;
 			return *this;
 		}
 		/**
@@ -196,7 +209,7 @@ public:
 		const_iterator operator++(int)
 		{
 			++index;
-			return const_iterator(index);
+			return const_iterator(arr, index);
 		}
 		/**
 		 * TODO ++iter
@@ -212,7 +225,7 @@ public:
 		const_iterator operator--(int)
 		{
 			--index;
-			return const_iterator(index);
+			return const_iterator(arr, index);
 		}
 		/**
 		 * TODO --iter
@@ -259,8 +272,8 @@ public:
 	 */
 	vector()
 	{
-		arr = new T[2];
-		arrSize = 2;
+		arr = new T[1];
+		arrSize = 1;
 		dataSize = 0;
 	}
 	vector(const vector &other)
@@ -268,8 +281,8 @@ public:
 		arrSize = other.arrSize;
 		arr = new T[arrSize];
 		dataSize = other.dataSize;
-		for (int i = 0; i < vector.dataSize; ++i)
-			arr[i] = vector.arr[i];
+		for (int i = 0; i < other.dataSize; ++i)
+			arr[i] = other.arr[i];
 	}
 	/**
 	 * TODO Destructor
@@ -290,61 +303,61 @@ public:
 			arr = new T[arrSize];
 		}
 		dataSize = other.dataSize;
-		for (int i = 0; i < vector.dataSize; ++i)
-			arr[i] = vector.arr[i];
+		for (int i = 0; i < other.dataSize; ++i)
+			arr[i] = other.arr[i];
 	}
 	/**
 	 * assigns specified element with bounds checking
-	 * throw index_out_of_bound if pos is not in [0, size)
+	 * throw index_out_of_bound() if pos is not in [0, size)
 	 */
 	T & at(const size_t &pos)
 	{
 		if (pos < 0 || pos >= dataSize)
-			throw index_out_of_bound;
+			throw index_out_of_bound();
 		return arr[pos];
 	}
 	const T & at(const size_t &pos) const
 	{
 		if (pos < 0 || pos >= dataSize)
-			throw index_out_of_bound;
+			throw index_out_of_bound();
 		return arr[pos];
 	}
 	/**
 	 * assigns specified element with bounds checking
-	 * throw index_out_of_bound if pos is not in [0, size)
+	 * throw index_out_of_bound() if pos is not in [0, size)
 	 * !!! Pay attentions
 	 *   In STL this operator does not check the boundary but I want you to do.
 	 */
 	T & operator[](const size_t &pos)
 	{
 		if (pos < 0 || pos >= dataSize)
-			throw index_out_of_bound;
+			throw index_out_of_bound();
 		return arr[pos];
 	}
 	const T & operator[](const size_t &pos) const
 	{
 		if (pos < 0 || pos >= dataSize)
-			throw index_out_of_bound;
+			throw index_out_of_bound();
 		return arr[pos];
 	}
 	/**
 	 * access the first element.
-	 * throw container_is_empty if size == 0
+	 * throw container_is_empty() if size == 0
 	 */
 	const T & front() const
 	{
 		if (empty())
-			throw container_is_empty;
+			throw container_is_empty();
 		return arr[0];
 	}
 	/**
 	 * access the last element.
-	 * throw container_is_empty if size == 0
+	 * throw container_is_empty() if size == 0
 	 */
 	const T & back() const
 	{
 		if (empty())
-			throw container_is_empty;
+			throw container_is_empty();
 		return arr[dataSize - 1];
 	}
 	/**
@@ -352,22 +365,22 @@ public:
 	 */
 	iterator begin()
 	{
-		return iterator(0);
+		return iterator(arr, 0);
 	}
 	const_iterator cbegin() const
 	{
-		return const_iterator(0);
+		return const_iterator(arr, 0);
 	}
 	/**
 	 * returns an iterator to the end.
 	 */
 	iterator end()
 	{
-		return iterator(dataSize - 1);
+		return iterator(arr, dataSize);
 	}
 	const_iterator cend() const
 	{
-		return const_iterator(dataSize - 1);
+		return const_iterator(arr, dataSize);
 	}
 	/**
 	 * checks whether the container is empty
@@ -403,31 +416,31 @@ public:
 	 */
 	iterator insert(iterator pos, const T &value)
 	{
-		if (dataSize + 1 == arrSize)
+		if (dataSize + 1 > arrSize)
 			expand();
 		++dataSize;
 		for (int i = dataSize - 1; i > pos.index; --i)
 			arr[i] = arr[i - 1];
 		arr[pos.index] = value;
-		return iterator(pos.index);
+		return iterator(arr, pos.index);
 	}
 	/**
 	 * inserts value at index ind.
 	 * after inserting, this->at(ind) == value is true
 	 * returns an iterator pointing to the inserted value.
-	 * throw index_out_of_bound if ind > size (in this situation ind can be size because after inserting the size will increase 1.)
+	 * throw index_out_of_bound() if ind > size (in this situation ind can be size because after inserting the size will increase 1.)
 	 */
 	iterator insert(const size_t &ind, const T &value)
 	{
-		if (ind > dataSize)
-			throw index_out_of_bound;
-		if (dataSize + 1 == arrSize)
+		if (ind < 0 || ind > dataSize)
+			throw index_out_of_bound();
+		if (dataSize + 1 > arrSize)
 			expand();
 		++dataSize;
 		for (int i = dataSize - 1; i > ind; --i)
 			arr[i] = arr[i - 1];
 		arr[ind] = value;
-		return iterator(ind);
+		return iterator(arr, ind);
 	}
 	/**
 	 * removes the element at pos.
@@ -439,21 +452,21 @@ public:
 		--dataSize;
 		for (int i = pos.index; i < dataSize; i++)
 			arr[i] = arr[i + 1];
-		return iterator(pos.index);
+		return iterator(arr, pos.index);
 	}
 	/**
 	 * removes the element with index ind.
 	 * return an iterator pointing to the following element.
-	 * throw index_out_of_bound if ind >= size
+	 * throw index_out_of_bound() if ind >= size
 	 */
 	iterator erase(const size_t &ind)
 	{
 		if (ind >= dataSize)
-			throw index_out_of_bound;
+			throw index_out_of_bound();
 		--dataSize;
 		for (int i = ind; i < dataSize; i++)
 			arr[i] = arr[i + 1];
-		return iterator(ind);
+		return iterator(arr, ind);
 	}
 	/**
 	 * adds an element to the end.
@@ -467,10 +480,12 @@ public:
 	}
 	/**
 	 * remove the last element from the end.
-	 * throw container_is_empty if size() == 0
+	 * throw container_is_empty() if size() == 0
 	 */
 	void pop_back()
 	{
+		if (empty())
+			throw container_is_empty();
 		--dataSize;
 	}
 };
